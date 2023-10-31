d6e68989cb0fa26496282844f660c7a11a747260
keshusun <144615236+keshusun@users.noreply.github.com>
2023-10-31 12:06:49 +0900

    Initial commit

---
+++ Assets/CharaController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharaController : MonoBehaviour
+{
+    private Rigidbody2D rigid2D;
+
+    private float velocityX = 5f;
+
+    private float velocityY = 10f;
+
+    public int MaxJumpCount = 2;
+
+    private float jumpCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputVelocityX = 0;
+
+        float inputVelovityY = 0;
+
+        if(Input.GetKey (KeyCode.LeftArrow))
+        {
+            inputVelocityX = -this.velocityX;
+        }
+        if(Input.GetKey (KeyCode.RightArrow))
+        {
+            inputVelocityX = this.velocityX;
+        }
+        if(Input.GetKeyDown (KeyCode.UpArrow) && this.jumpCount < MaxJumpCount)
+        {
+            inputVelovityY = velocityY;
+            jumpCount++;
+        }
+        else
+        {
+            inputVelovityY = this.rigid2D.velocity.y;
+        }
+        
+
+
+        this.rigid2D.velocity = new Vector3(inputVelocityX, inputVelovityY, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "EarthTag")
+        {
+            jumpCount = 0;
+        }
+    }
+
+
+}
+
---
+++ Assets/My CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour
+{
+    private GameObject mainChara;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.mainChara = GameObject.Find("mainChara");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = new Vector3(this.mainChara.transform.position.x, this.mainChara.transform.position.y , -10);
+    }
+}
